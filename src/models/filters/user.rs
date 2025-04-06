aca20f286b15eca5f037fe740f50e35d5e43e7fa
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2025-04-06 18:50:49 -0400

    Removed filters

--- src/models/filters/user.rs
+++
@@ -1,6 +0,0 @@
-pub struct UserFilter {
-    pub id: Option<uuid::Uuid>,
-    pub username: Option<String>,
-    pub password: Option<String>,
-    pub created_at: Option<chrono::DateTime<chrono::Utc>>,
-}
